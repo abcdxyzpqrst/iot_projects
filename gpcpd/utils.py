9528974ed6f7b34ede08cd53bbe750bb78d87fc1
Hajin Shim <shimazing@kaist.ac.kr>
2018-07-04 14:08:00 +0900

    upm prediction (f_mean and f_var) with given kernel and data

--- gpcpd/utils.py
+++ gpcpd/utils.py
@@ -1,5 +1,9 @@
 import torch
 from torch.nn import Module
+try:
+    from multitaskgp import kron_prod
+except:
+    from .multitaskgp import kron_prod
 
 class IncrementalCholesky(Module):
     def __init__(self, A):
@@ -13,6 +17,14 @@ class IncrementalCholesky(Module):
         [ A  B.t()]
         [ B    D  ]
         when we know the L_A s.t L_A L_A.t() = A
+
+        L_updated =
+        [L_A    0 ]
+        [B_    L_S] s.t
+        B_ = B(L_A^-1).t()
+        L_S = potrf(S) where S = D - BA^(-1)B.t()
+
+        return B_, L_S
         '''
         n = self.A.shape[0]
         m = D.shape[0]
@@ -33,7 +45,7 @@ class IncrementalCholesky(Module):
             torch.cat((A, B.t()), dim=1),
             torch.cat((B, D), dim=1)), dim=0)
 
-        return self.L_A, self.A
+        return B_, L_S
 
 def flip(x, dim):
     dim = x.dim() + dim if dim < 0 else dim
@@ -42,30 +54,57 @@ def flip(x, dim):
              for i in range(x.dim()))
     return x[inds]
 
-def calc_f_mean_var(X_prev, Y_prev, X_curr, kernel, task_kernel):
+
+def calc_f_mean_var(X_prev, Y_prev, X_curr, kernel, task_kernel=None):
+    '''
+    iteratively calculate f_mean and f_var
+
+    when we already know x1 (= torch.gesv(y1, L_A))
+
+    Let
+    [L_A    0 ] [x1] = [y1]
+    [B_    L_S] [x2]   [y2],
+    --> x2 = torch.gesv(y2 - B_*x1, L_S)
+    f_mean and f_var are updated additively
+
+    '''
+    # T <= window size, it is reflected when the inputs are passed
     T = X_prev.shape[0]
     D = X_prev.shape[1]
     l = Y_prev.shape[1]
     assert T == Y_prev.shape[0]
-
     # reverse order
     reverse_ind = torch.arange(T-1, -1, -1).long()
     X_prev_rev = X_prev[reverse_ind]
     Y_prev_rev = Y_prev[reverse_ind]
-
 
     K_ss = kernel(X_curr)
     K_star = kernel(X_prev_rev, X_curr)
     K = kernel(X_prev_rev, X_prev_rev)
 
     if l > 1:
+        assert task_kernel is not None
         # TODO multitask case
-        pass
+        K = kron_prod(task_kernel, K)
+        K_ss = kron_prod(task_kernel, K_ss)
+        K_star = kron_prod(task_kernel, K_star)
 
     cholesky = IncrementalCholesky(K[:l, :l]) # most recent
-    for t in range(T):
-        pass
+    L = cholesky.L_A
+    A, V = torch.gesv(K_star[:l], L)[0], torch.gesv(Y_prev_rev[:1].t(), L)[0]
 
+    f_means = [torch.mm(A.t(), V)]
+    f_vars = [torch.mm(A.t(), A)]
+
+    for t in range(1, T):
+        B_, L_S = cholesky.update(K[t*l:(t+1)*l, :t*l], K[t*l:(t+1)*l, t*l:(t+1)*l])
+        A = torch.gesv(K_star[t*l:(t+1)*l] - B_.mm(A), L_S)[0]
+        V = torch.gesv(Y_prev_rev[t:t+1].t() - B_.mm(V), L_S)[0]
+        f_means.append(f_means[-1] + torch.mm(A.t(), V))
+        f_vars.append(f_vars[-1] + torch.mm(A.t(), A))
+
+    # order r_t== 1 ~ r_t == t
+    return f_means, f_vars
 
 
 if __name__ == '__main__':
